13622d1a11dc5e1a560f4ff00753da4714609564
jballanger <ballanger.julien@gmail.com>
2018-05-09 23:34:16 +0200

    Add checkChannels and setMatrixData

--- src/search.cc
+++ src/search.cc
@@ -34,11 +34,55 @@ void        checkProps(napi_env env, napi_value obj) {
   checkSingleProp(env, obj, createString(env, "data"));
 }
 
+void        checkChannels(napi_env env, napi_value m1, napi_value m2) {
+  napi_value  m1ChannelsValue;
+  napi_value  m2ChannelsValue;
+  uint32_t    m1Channels;
+  uint32_t    m2Channels;
+  napi_status status;
+
+  status = napi_get_property(env, m1, createString(env, "channels"), &m1ChannelsValue);
+  if (status != napi_ok) {
+    napi_throw_error(env, nullptr, "Couldn't get m1 channels property.");
+  }
+  status = napi_get_value_uint32(env, m1ChannelsValue, &m1Channels);
+  if (status != napi_ok) {
+    napi_throw_error(env, nullptr, "Couldn't get uint32 value.");
+  }
+  status = napi_get_property(env, m2, createString(env, "channels"), &m2ChannelsValue);
+  if (status != napi_ok) {
+    napi_throw_error(env, nullptr, "Couldn't get m2 channels property.");
+  }
+  status = napi_get_value_uint32(env, m2ChannelsValue, &m2Channels);
+  if (status != napi_ok) {
+    napi_throw_error(env, nullptr, "Couldn't get uint32 value.");
+  }
+  if (m1Channels < 1 || m1Channels > 4 || m2Channels < 1 || m2Channels > 4) {
+    napi_throw_error(env, nullptr, "Bad number of channels.");
+  }
+  if (abs(m1Channels - m2Channels) > 1) {
+    napi_throw_error(env, nullptr, "Channel mismatch.");
+  }
+}
+
+void        setMatrixData(napi_env env, napi_value obj, napi_value *matrix) {
+  napi_value  dataValue;
+  napi_status status;
+
+  status = napi_get_property(env, obj, createString(env, "data"), &dataValue);
+  if (status != napi_ok) {
+    napi_throw_error(env, nullptr, "Couldn't get data property.");
+  }
+  *matrix = dataValue;
+}
+
 napi_value  Search(napi_env env, napi_callback_info info) {
   size_t      argc = 2;
   napi_value  argv[2];
   napi_value  m1;
   napi_value  m2;
+  napi_value  m1Data;
+  napi_value  m2Data;
   napi_status status;
 
   status = napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
@@ -51,7 +95,10 @@ napi_value  Search(napi_env env, napi_callback_info info) {
   m2 = argv[1];
   checkProps(env, m1);
   checkProps(env, m2);
-  return m1;
+  checkChannels(env, m1, m2);
+  setMatrixData(env, m1, &m1Data);
+  setMatrixData(env, m2, &m2Data);
+  return m1Data;
 }
 
 napi_value  init(napi_env env, napi_value exports) {
